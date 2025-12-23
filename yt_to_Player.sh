8e41e99a7779806961cea441b775ba0d8663c876
212-del <74853481+212-del@users.noreply.github.com>
2025-12-24 00:38:06 +0530

    Update yt_to_Player.sh

--- yt_to_Player.sh
+++ yt_to_Player.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+cd
+if ! [[ ls -d */ | grep "trash" ]]; then
+  mkdir trash
+ else
+  echo " Trash Folder Already Exist!!"
+fi
+sudo chmod -R u+rw trash/
 read -r -p "Enter the youtube video link: " link
 read -r -p "Enter the filename: " filename
 yt-dlp -x --audio-format mp3 -o "trash/${filename}.%(ext)s"  $link &&  ffplay trash/${filename}.mp3
