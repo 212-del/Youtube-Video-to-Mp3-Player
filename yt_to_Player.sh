b198bd4b4c52c9e9253e5573e991ee165e4a6542
212-del <74853481+212-del@users.noreply.github.com>
2026-02-06 21:01:08 +0530

    Enhance yt_to_Player.sh for yt-dlp installation
    Updated script to install and update yt-dlp, modified trash directory handling, and changed audio extraction command.

--- yt_to_Player.sh
+++ yt_to_Player.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+ #add a section to install yt-dlp and update it.
 cd /tmp/
-if ! [[ ls -d */ | grep "trash" ]]; then
+if  [[ ls -d /tmp/* | grep "trash" ]]; then
   mkdir trash
  else
   echo " Trash Folder Already Exist!!"
 fi
-sudo chmod -R u+rw trash/
+sudo chmod -R u+rw /tmp/trash/
 read -r -p "Enter the youtube video link: " link
 read -r -p "Enter the filename: " filename
-yt-dlp -x --audio-format mp3 -o "trash/${filename}.%(ext)s"  $link &&  ffplay trash/${filename}.mp3
+echo "If you are gettin any error do update your yt-dlp"
+yt-dlp  --cookies-from-browser brave --js-runtimes node -x --audio-format mp3 -o "/tmp/trash/${filename}.%(ext)s"  ${link} ;  mpv /tmp/trash/${filename}.mp3
 
