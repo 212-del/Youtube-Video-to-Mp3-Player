c45f1511b809bb42973c02ce0d1789d32b9468e4
212-del <74853481+212-del@users.noreply.github.com>
2026-02-16 08:35:42 +0530

    Enhance audio playback with ffplay fallback
    Added fallback to ffplay if mpv fails to play the audio.

--- yt_to_Player.sh
+++ yt_to_Player.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
  # ===============================
+# Install FFmpeg
+# ===============================
+sudo apt install ffmpeg
+ 
+ # ===============================
 # Install or update yt-dlp
 # ===============================
 
@@ -37,5 +42,5 @@ sudo chmod -R u+rw /tmp/trash/
 read -r -p "Enter the youtube video link: " link
 read -r -p "Enter the filename: " filename
 echo "If you are gettin any error do update your yt-dlp"
-yt-dlp  --cookies-from-browser brave --js-runtimes node -x --audio-format mp3 -o "/tmp/trash/${filename}.%(ext)s"  ${link} ;  mpv /tmp/trash/${filename}.mp3
+yt-dlp  --cookies-from-browser brave --js-runtimes node -x --audio-format mp3 -o "/tmp/trash/${filename}.%(ext)s"  ${link} ;  mpv || ffplay /tmp/trash/${filename}.mp3
 
